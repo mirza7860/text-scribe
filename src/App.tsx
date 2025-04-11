68bcd60e5b35331d985390b733335def0be9be49
Mirza Sahil Alli Baig <mirzasahilallibaig@gmail.com>
2025-04-11 10:11:19 +0530

    remove about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -6,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Index from "./pages/Index";
-import About from "./pages/About";
 import Summaries from "./pages/Summaries";
 import NotFound from "./pages/NotFound";
 import Navbar from "./components/layout/Navbar";
@@ -24,7 +22,6 @@ const App = () => (
           <Route path="/" element={<Landing />} />
           <Route path="/process" element={<Index />} />
           <Route path="/summaries" element={<Summaries />} />
-          <Route path="/about" element={<About />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
